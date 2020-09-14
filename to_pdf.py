e2b78daa4c8d943b99700eaa69b254a38f95bd30
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-09-14 20:16:20 +0530

    Formatted the Script file with autopep8

--- to_pdf.py
+++ to_pdf.py
@@ -25,33 +25,35 @@ from PIL import Image
 from pathlib import Path
 from sys import argv
 
+
 def to_rgb(img):
-  """This function converts and Returns Image Objects with RGBA to RGB
-  It does not change the size (width and height) of the Image Object
+    """This function converts and Returns Image Objects with RGBA to RGB
+    It does not change the size (width and height) of the Image Object
 
-  Args:
-      img (Image Object): With RGBA or RGB channel
+    Args:
+        img (Image Object): With RGBA or RGB channel
 
-  Returns:
-      Image Object: With RGB channels only
-  """
+    Returns:
+        Image Object: With RGB channels only
+    """
 
-  
-  # If Image does not have Alha Channel
-  if len(img.split()) != 4:
-    # Return it without doing anything
-    return img
-  
-  # Create New Image Object with White BG
-  im = Image.new('RGB', img.size, (255,255,255))
-  im.paste(img, mask = img.split()[3])
-  return im
+    # If Image does not have Alha Channel
+    if len(img.split()) != 4:
+        # Return it without doing anything
+        return img
+
+    # Create New Image Object with White BG
+    im = Image.new('RGB', img.size, (255, 255, 255))
+    im.paste(img, mask=img.split()[3])
+    return im
+
 
 if len(argv) != 3:
-  print('[ERROR] Invalid number of arguments given')
-  print(f'[USAGE] python3 {Path(__file__).name} <path_to_images> <destination_path>')
-  print(f'[Example] python3 {Path(__file__).name} ~/Pictures Output.pdf')
-  exit(1)
+    print('[ERROR] Invalid number of arguments given')
+    print(
+        f'[USAGE] python3 {Path(__file__).name} <path_to_images> <destination_path>')
+    print(f'[Example] python3 {Path(__file__).name} ~/Pictures Output.pdf')
+    exit(1)
 
 # Path of image files (Second Command Line Argument)
 path = Path(argv[1])
@@ -63,20 +65,21 @@ img_list = []
 IMG_EXTENSIONS = ["jpg", "jpeg", "png", "bmp", "ico"]
 
 for image in path.iterdir():
-  # Select only JPG files
-  if image.suffix[1:] in IMG_EXTENSIONS:
-    img_list.append(image)
+    # Select only JPG files
+    if image.suffix[1:] in IMG_EXTENSIONS:
+        img_list.append(image)
 
 # if img_list is empty, exit
 if not img_list:
     print('[ERROR] Images not found! Please Make sure the specified path contains Images')
     exit(1)
 
-img_list.sort(key = lambda x: x.name)
+img_list.sort(key=lambda x: x.name)
 
 # Replacing Path objects with Image objects
 img_list = [to_rgb(Image.open(im.absolute())) for im in img_list]
 
 # Save First Image as PDF with All Other Images Appended to it
-img_list[0].save(pdf_filename, "PDF", resolution=100.0, save_all = True, append_images=img_list[1:])
+img_list[0].save(pdf_filename, "PDF", resolution=100.0,
+                 save_all=True, append_images=img_list[1:])
 print(f'{len(img_list)} Images saved to {pdf_filename} Successfully!!')
